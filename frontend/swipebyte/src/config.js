25ff0a757f7b775a388cfd25dc5d8e6f2dfa1c6c
daniel868905 <daniel868905@gmail.com>
2025-08-13 11:57:21 -0400

    adjusting aws end point

--- frontend/swipebyte/src/config.js
+++ frontend/swipebyte/src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  'https://u5dnmrdvhspwktiq6f2h76bh7q0vfxrt.lambda-url.us-east-2.on.aws/' || 'http://localhost:8000';
+  'https://u5dnmrdvhspwktiq6f2h76bh7q0vfxrt.lambda-url.us-east-2.on.aws/swipebyte' || 'http://localhost:8000';
