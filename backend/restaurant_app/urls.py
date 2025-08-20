d517ccb9300ed079dd3394d85cf16105b8abcc84
daniel868905 <daniel868905@gmail.com>
2025-08-20 12:15:40 -0400

    hot fix number 2

--- backend/restaurant_app/urls.py
+++ backend/restaurant_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import restaurant_photo,  restaurant_search, RestaurantSearchView
+from .views import restaurant_photo, restaurant_search, RestaurantSearchView
 
 urlpatterns = [
-    path('restaurants/photo/', restaurant_photo, name='restaurant-photo'),
-    path('', restaurant_search, name='restaurant-search')
+    path('photo/', restaurant_photo, name='restaurant-photo'),
+    path('', restaurant_search, name='restaurant-search'),
 ]
